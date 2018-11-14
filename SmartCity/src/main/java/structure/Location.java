60ec356ae42eb8c1740e695d3a4b98ef7e7b3d59
Jaime Huarsaya Rivera <jaime.huarsaya.rivera@gmail.com>
2018-11-13 19:07:03 -0500

    Fix structure Location

--- SmartCity/src/main/java/structure/Location.java
+++ SmartCity/src/main/java/structure/Location.java
@@ -2,6 +2,7 @@ package structure;
 
 import java.util.Date;
 
+import com.googlecode.objectify.Key;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 import com.googlecode.objectify.annotation.Index;
@@ -9,7 +10,7 @@ import com.googlecode.objectify.annotation.Index;
 @Entity
 public class Location {
 	@Id Long id;
-	@Index String vehicle_id;
+	@Index Key<Vehicle> vehicle;
 	Date datetime2;
 	@Index double latitude;
 	@Index double longitude;
